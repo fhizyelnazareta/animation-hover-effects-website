8ec31f0788dd7f1e40b66ada616455466d3ba12a
Fhizyel Nazareta <fhizyelblawers@gmail.com>
2020-09-20 07:39:56 +0700

    menambah komentar pada file script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
+/* Script Effect for animation*/
 document.addEventListener('mousemove', function (e) {
     const body = document.querySelector('body');
     const bubbles = document.createElement('span');
+
+    // penerapan effect
     bubbles.style.left = -75 + e.offsetX + 'px';
     bubbles.style.top = -75 + e.offsetY + 'px';
+
+    // menambahkan element baru pada body
     body.appendChild(bubbles);
 });
+
